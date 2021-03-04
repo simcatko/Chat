f362aa99bd229d690aeff15ae8972db8f6ca8087
simcatko <simona.prokopova2@gmail.com>
2021-03-04 22:54:51 +0100

    clients com

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -6,35 +6,25 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-
     public static void main(String[] args) {
         System.out.println("Server");
 
         int portNumber = Integer.parseInt(args[0]);
+        List<ServerClient> clients = new ArrayList<>();
 
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Nove spojenie: " + clientSocket.getInetAddress().toString());
+            int i = 10;
+            while (i-- >0) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nove spojenie: " + clientSocket.getInetAddress().toString());
+                ServerClient client = new ServerClient(clientSocket, clients);
+                client.start();
 
-            PrintWriter out =
-                    new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
-
-
-            String inputLine, outputLine;
-
-            // Initiate conversation with client
-            out.println("Ahoj client");
-
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = inputLine;
-                out.println(outputLine);
-                if (outputLine.equals("Bye."))
-                    break;
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/com/company/ServerClient.java
+++ src/com/company/ServerClient.java
@@ -6,31 +6,45 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class ServerClient extends Thread {
     private final Socket socket;
+    private final List<ServerClient> clients;
+    private BufferedReader in;
+    private PrintWriter out;
 
-    ServerClient(Socket socket) {
+    ServerClient(Socket socket, List<ServerClient> clients) {
         this.socket = socket;
+        this.clients = clients;
+    }
+
+    public void println(String x) {
+        System.out.println("Sending message to client");
+        this.out.println(x);
     }
 
     public void run() {
         try {
-
-
             System.out.println("MyThread running");
-            PrintWriter out =
+            this.out =
                     new PrintWriter(this.socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
+            this.in = new BufferedReader(
                     new InputStreamReader(this.socket.getInputStream()));
+            this.clients.add(this);
             String inputLine, outputLine;
 
             // Initiate conversation with client
             out.println("Ahoj client");
-
+            System.out.println("debug");
             while ((inputLine = in.readLine()) != null) {
                 outputLine = inputLine;
-                out.println(outputLine);
+
+                for (ServerClient peer : this.clients) {
+                    if (peer != this) {
+                        peer.println(outputLine);
+                    }
+                }
                 if (outputLine.equals("Bye."))
                     break;
             }
